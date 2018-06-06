699d37525a2b9e16c39cbf720633e4210ed97583
乐怡胡 <leyihu@leyihudeMacBook-Pro.local>
2018-06-06 00:47:15 -0400

    message

--- services/passport.js
+++ services/passport.js
@@ -1,22 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 const mongoose = require('mongoose');
-const keys = require('../config/keys.js');
 
 const User = mongoose.model('users');
 
 passport.use(
-  new GoogleStrategy(
-   {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy:true
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken,refreshToken, profile, done) => {
+    User.findOne({googleId: profile.id }).then((existingUser) => {
+        if(existingUser) {
+          //have a record
+          done(null, existingUser);
+        } else {console.log("execute else");
+          // we don't have record
+          new User({googleId: profile.id})
+          .save()
+          .then(user => done(null,user));
+          }
+        })
+      }
+   )
 );
